458edb234542a26729c7641cfd38612017a87eeb
Juan Herrera <juan.herrerati@gmail.com>
2023-06-16 09:22:48 -0400

    Instala nuevas dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public'),
     }),
